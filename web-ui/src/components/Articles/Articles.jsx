2465c8d2f1a5723f47fec23564645498217c6801
Anaya Bussey <anayabussey@gmail.com>
2023-11-24 14:47:22 -0500

    Blank

--- web-ui/src/components/Articles/Articles.jsx
+++ web-ui/src/components/Articles/Articles.jsx
@@ -6,11 +6,11 @@ function Articles() {
         <div id='arc'>
             <div>
                 {/* <img id="bates" src={bates}/> */}
-                <a href="/articles/0"><p>Bates Holds Annual Family Weekend</p></a>
+                <a href="/articles/0" target='_blank'><p>Bates Holds Annual Family Weekend</p></a>
             </div>
             <div>
                 {/* <img id="bates" src={bates}/> */}
-                <a href="/articles/1"><p>How Far I've Gone and Where I'm Going</p></a>
+                <a href="/articles/1" target='_blank'><p>How Far I've Gone and Where I'm Going</p></a>
             </div>
         </div>
     )
