f07752476094f4b5df3c63356a388faad02aa87c
Anaya Bussey <anayabussey@gmail.com>
2023-11-24 15:01:43 -0500

    Break

--- web-ui/src/components/Home/Home.jsx
+++ web-ui/src/components/Home/Home.jsx
@@ -14,25 +14,34 @@ function Home() {
             <Navbar/>
             <Hero/>
             <div id="label">
+                <br/>
+                <br/>
                 <h3>About Me</h3>
             </div>
-            
             <AboutMe/>
             <div id="label1">
+                <br/>
+                <br/>
                 <h3>Editing Work</h3>
             </div>
 
             <Editing/>
             <div id="label2">
+                <br/>
+                <br/>
                 <h3>Creative Fiction</h3>
             </div>
             <CreatFic/>
             <div id="label3">
+                <br/>
+                <br/>
                 <h3>Articles</h3>
                 <img id="bates2" src={bates}/>
             </div>
             <Articles/>
             <div id="label4">
+                <br/>
+                <br/>
                 <h3>Resume</h3>
             </div>
             <Resume/>
