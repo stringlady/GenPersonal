a970579aabc727d11b42fbbccf306d4d5f424364
Anaya Bussey <anayabussey@gmail.com>
2025-06-03 23:37:58 -0400

    Revert "New Article"
    This reverts commit 6911b6e317b92f33324bea904ccb9c310b51420d.

--- web-ui/src/components/Articles/A_Full.jsx
+++ web-ui/src/components/Articles/A_Full.jsx
@@ -2,7 +2,7 @@ import './A_Full.css'
 import { useParams } from 'react-router-dom'
 import arc1 from '../../assets/arc1.pdf'
 import arc2 from '../../assets/arc2.pdf'
-import arc3 from '../../assets/arc3_2.pdf'
+import arc3 from '../../assets/arc3.pdf'
 
 function A_Full() {
     let { url } = useParams();
